0b1767762b1e896fc461160f0b2c5ef2b9d13436
Bryce Berger <bryce.z.berger@gmail.com>
2025-01-12 03:19:38 -0500

    make buildable with `nix build`

--- flake.nix
+++ flake.nix
@@ -1,6 +1,7 @@
 {
   inputs = {
     nixpkgs.url = "nixpkgs";
+    flake-utils.url = "github:numtide/flake-utils";
     fenix = {
       url = "github:nix-community/fenix";
       inputs.nixpkgs.follows = "nixpkgs";
@@ -11,17 +12,30 @@
     self,
     nixpkgs,
     fenix,
-  }: let
-    system = "x86_64-linux";
-    pkgs = nixpkgs.legacyPackages.${system};
-    toolchain = fenix.packages.${system}.complete.toolchain;
-  in {
-    devShell.${system} = pkgs.mkShell {
-      name = "jj-manage";
-      packages = with pkgs; [
-        toolchain
-        cargo-nextest
-      ];
-    };
-  };
+    flake-utils,
+  }:
+    flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = nixpkgs.legacyPackages.${system};
+      toolchain = fenix.packages.${system}.complete.toolchain;
+      ourRustPlatform = pkgs.makeRustPlatform {
+        rustc = toolchain;
+        cargo = toolchain;
+      };
+    in {
+      packages.jujutsu-manage = ourRustPlatform.buildRustPackage {
+        pname = "jujutsu-manage";
+        version = "unstable-${self.shortRev or "dirty"}";
+        src = ./.;
+        cargoLock.lockFile = ./Cargo.lock;
+      };
+      packages.default = self.packages.${system}.jujutsu-manage;
+
+      devShell = pkgs.mkShell {
+        name = "jj-manage";
+        packages = with pkgs; [
+          toolchain
+          cargo-nextest
+        ];
+      };
+    });
 }
